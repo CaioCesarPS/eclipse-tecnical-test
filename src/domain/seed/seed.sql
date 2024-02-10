22056fbc8b86963e215512e68049f5f919a3e50e
CaioCPS <caio.cesar@ducash.com.br>
2024-02-10 10:00:54 -0300

    fix: fix sql seed to create tables and relacions

--- src/domain/seed/seed.sql
+++ src/domain/seed/seed.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS public.wallets (
   id SERIAL PRIMARY KEY,
   balance integer NOT NULL DEFAULT 0,
   client_id integer NOT NULL,
-  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS public.coins_to_wallet (
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS public.coins_to_wallet (
   coin_id integer,
   wallet_id integer,
   coin_quantity integer,
-  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS public.offers (
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS public.offers (
   coin_quantity integer NOT NULL,
   total_offer_value integer NOT NULL,
   active boolean NOT NULL DEFAULT true,
-  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS public.client_favorites_offers (
@@ -66,13 +66,13 @@ INSERT INTO public.coins (name, current_price) VALUES ('Ethereum', 200);
 INSERT INTO public.coins (name, current_price) VALUES ('Litecoin', 300);
 
 --Create Wallets
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (1, 1000, 1);
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (2, 2000, 1);
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (3, 3000, 1);
+INSERT INTO public.wallets (balance, client_id) VALUES (1000, 1);
+INSERT INTO public.wallets (balance, client_id) VALUES (2000, 1);
+INSERT INTO public.wallets (balance, client_id) VALUES (3000, 1);
 
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (1, 1000, 2);
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (2, 2000, 2);
-INSERT INTO public.wallets (coin_id, balance, client_id) VALUES (3, 3000, 2);
+INSERT INTO public.wallets (balance, client_id) VALUES (1000, 2);
+INSERT INTO public.wallets (balance, client_id) VALUES (2000, 2);
+INSERT INTO public.wallets (balance, client_id) VALUES (3000, 2);
 
 INSERT INTO public.coins_to_wallet (coin_id, wallet_id, coin_quantity) VALUES (1, 1, 10);
 INSERT INTO public.coins_to_wallet (coin_id, wallet_id, coin_quantity) VALUES (2, 1, 20);
